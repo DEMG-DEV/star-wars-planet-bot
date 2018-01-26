4b797c8ca2272e2826bd02d92060c2ed613e8733
David Mendez Guardado <demg@outlook.com>
2018-01-26 10:03:48 -0600

    -add the hashtag "#StarWars"

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ if(opc == 1):
 		film = swapi.get_film(randint(1,7))
 	except:
 		film = swapi.get_film(1)
-	tweet = "Title: "+film.title+"\nDirector: "+film.director+"\nRelease Date: "+film.release_date+"\nOpening Crawl:"
+	tweet = "#StarWars\nTitle: "+film.title+"\nDirector: "+film.director+"\nRelease Date: "+film.release_date+"\nOpening Crawl:"
 	if(len(tweet) < 276):
 		ocL = len(film.opening_crawl)
 		tL = len(tweet)
@@ -28,27 +28,27 @@ elif(opc == 2):
 	except:
 		person = swapi.get_person(1)
 	home = person.get_homeworld()
-	tweet = "Name: "+person.name+"\nHeight: "+person.height+"\nMass: "+person.mass+"\nHair Color: "+person.hair_color+"\nSkin Color: "+person.skin_color+"\nEye Color: "+person.eye_color+"\nBirth Year: "+person.birth_year+"\nGender: "+person.gender+"\nHomeworld: "+home.name
+	tweet = "#StarWars\nName: "+person.name+"\nHeight: "+person.height+"\nMass: "+person.mass+"\nHair Color: "+person.hair_color+"\nSkin Color: "+person.skin_color+"\nEye Color: "+person.eye_color+"\nBirth Year: "+person.birth_year+"\nGender: "+person.gender+"\nHomeworld: "+home.name
 elif(opc == 3):
 	#planets
 	try:
 		planet = swapi.get_planet(randint(1, 61))
 	except:
 		planet = swapi.get_planet(1)
-	tweet = "Planet\nName: " + planet.name+"\nRotation Period: "+planet.rotation_period+"\nOrbital Period: "+planet.orbital_period+"\nDiameter: "+planet.diameter+"\nClimate:"+planet.climate+"\nGravity: "+planet.gravity+"\nTerrain: "+planet.terrain+"\nSurface_water:" +planet.surface_water+"\nPopulation:" +planet.population
+	tweet = "#StarWars\nPlanet\nName: " + planet.name+"\nRotation Period: "+planet.rotation_period+"\nOrbital Period: "+planet.orbital_period+"\nDiameter: "+planet.diameter+"\nClimate:"+planet.climate+"\nGravity: "+planet.gravity+"\nTerrain: "+planet.terrain+"\nSurface_water:" +planet.surface_water+"\nPopulation:" +planet.population
 elif(opc == 4):
 	#starships
 	try:
 		starship = swapi.get_starship(randint(1,37))
 	except:
 		starship = swapi.get_starship(10)
-	tweet = "Name: "+starship.name+"\nModel: "+starship.model+"\nManufacturer: "+starship.manufacturer+"\nMax Atmosphering Speed: "+starship.max_atmosphering_speed+"\nStarship Class: "+starship.starship_class+"\nCargo Capacity: "+starship.cargo_capacity
+	tweet = "#StarWars\nName: "+starship.name+"\nModel: "+starship.model+"\nManufacturer: "+starship.manufacturer+"\nMax Atmosphering Speed: "+starship.max_atmosphering_speed+"\nStarship Class: "+starship.starship_class+"\nCargo Capacity: "+starship.cargo_capacity
 elif(opc == 5):
 	#vehicles
 	try:
 		vehicle = swapi.get_vehicle(randint(1,39))
 	except:
 		vehicle = swapi.get_vehicle(14)
-	tweet = "Name: "+vehicle.name+"\nModel :"+vehicle.model+"\nManufacturer :"+vehicle.manufacturer+"\nMax Atmosphering Speed: "+vehicle.max_atmosphering_speed+"\nVehicle Class: "+vehicle.vehicle_class+"\nCargo Capacity: "+vehicle.cargo_capacity
+	tweet = "#StarWars\nName: "+vehicle.name+"\nModel :"+vehicle.model+"\nManufacturer :"+vehicle.manufacturer+"\nMax Atmosphering Speed: "+vehicle.max_atmosphering_speed+"\nVehicle Class: "+vehicle.vehicle_class+"\nCargo Capacity: "+vehicle.cargo_capacity
 	
 api.update_status(tweet)
