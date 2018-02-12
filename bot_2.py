ca9461585610226c53625f47ddf24de4f85fd160
David Mendez Guardado <demg@outlook.com>
2018-02-12 12:34:09 -0600

    -some details.

--- bot_2.py
+++ bot_2.py
@@ -64,10 +64,9 @@ try:
 	if request.status_code == 200:
 		with open(filename, 'wb') as image:
 			for chunk in request:
-	       			image.write(chunk)
-
-	    	api.update_with_media(filename, status=message)
-	    	os.remove(filename)
+	       		image.write(chunk)
+	    api.update_with_media(filename, status=message)
+		os.remove(filename)
 	else:
 		print("Unable to download image")
 except:
